144fcfeaac979ad8582119689cf97e2c05440cd2
rohan <rohan13maharjan@gmail.com>
2025-05-19 12:37:50 +0545

    Updated mapper classes to handle the input and output DTOs more elegantly

--- src/main/java/com/pgl1/database/mapper/OrderMapper.java
+++ src/main/java/com/pgl1/database/mapper/OrderMapper.java
@@ -1,4 +1,27 @@
 package com.pgl1.database.mapper;
 
+import com.pgl1.database.dto.request.order.OrderUpdateDTO;
+import com.pgl1.database.dto.request.order.OrderWriteDTO;
+import com.pgl1.database.dto.response.order.OrderReadDTO;
+import com.pgl1.database.model.entity.Order;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface OrderMapper {
+    @Mapping(target="orderId", ignore = true)
+    @Mapping(target="pickupPoint.pickupPointId", source = "pickupPointId")
+    @Mapping(target = "user.userId", source = "userId")
+    Order orderWriteDTOToOrder(OrderWriteDTO orderWriteDTO);
+
+    @Mapping(target="pickupPoint.pickupPointId", source = "pickupPointId")
+    @Mapping(target = "user.userId", source = "userId")
+    Order orderUpdateDTOToOrder(OrderUpdateDTO orderUpdateDTO);
+
+    @Mapping(target = "userId", source = "user.userId")
+    @Mapping(target = "pickupPointId", source = "pickupPoint.pickupPointId")
+    OrderReadDTO orderToOrderReadDTO(Order order);
+
 }
+
+
--- src/main/java/com/pgl1/database/mapper/UserMapper.java
+++ src/main/java/com/pgl1/database/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.pgl1.database.mapper;
 
+import com.pgl1.database.dto.request.user.UserUpdateDTO;
 import com.pgl1.database.dto.request.user.UserWriteDTO;
 import com.pgl1.database.dto.response.user.UserReadDTO;
 import com.pgl1.database.model.entity.Order;
@@ -16,13 +17,18 @@ public interface UserMapper {
 
     @Mapping(target="userId", ignore = true)
     @Mapping(target = "orders", source = "orders", qualifiedByName = "emptyListIfNull")
+    @Mapping(target="locationId", source = "location")
     User userWriteDTOToUser(UserWriteDTO userWriteDTO);
 
     @Mapping(target = "userId", source = "userId")
     @Mapping(target = "userName", source = "userName")
     @Mapping(target = "userPhone", source = "userPhone")
-    @Mapping(target = "userLocation", source = "userLocation")
     UserReadDTO userToUserReadDTO(User user);
+
+    @Mapping(target="userId", source="userId")
+    @Mapping(target = "orders", source = "orders", qualifiedByName = "emptyListIfNull")
+    @Mapping(target = "locationId", source = "location")
+    User userUpdateDTOToUser(UserUpdateDTO userUpdateDTO);
 
     @Named("emptyListIfNull")
     default List<Order> emptyListIfNull(List<Order> list) {
